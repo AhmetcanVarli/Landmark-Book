5febd135839e36b00e517a0946dc4869a760dc27
AhmetcanVarli <AhmetcanVarli@github.com>
2020-10-01 15:06:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/landmarkbook/DetailActivity.java
@@ -0,0 +1,34 @@
+package com.example.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView landmarkNameText = findViewById(R.id.landmarkNameText);
+        TextView countryNameText = findViewById(R.id.countryNameText);
+
+        Intent intent = getIntent();
+        String landmarkName = intent.getStringExtra("name");
+        String countrName = intent.getStringExtra("country");
+        landmarkNameText.setText(landmarkName);
+        countryNameText.setText(countrName);
+        Singleton singleton = Singleton.getInstance();
+        imageView.setImageBitmap(singleton.getChosenImage());
+
+
+        //imageView.setImageBitmap(selectedImage);
+    }
+}
